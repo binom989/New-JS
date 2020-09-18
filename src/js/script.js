5f8bf3b4906464294840d04060b734e89ac1ad41
binom989 <binom9@yandex.ru>
2020-09-18 15:57:59 +0400

    18.09

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,49 @@
-"use strict";  // используем современный режим скрипта
+//"use strict";  // используем современный режим скрипта
 
-let number = 5;
-const leftBorderWidth = 1;
-number = 10;
-console.log(number);
+/* Задание на урок:
 
-const person = "Alex";
+    1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+    'Сколько фильмов вы уже посмотрели?' */
 
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+console.log(numberOfFilms);
 
-const obj = {
-    name: "John",
-    age: 25,
-    isMarried: false   //женат
+/*  2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false */
+
+    let personalMovieDB = {
+        count: numberOfFilms, //сюда передается ответ на первый вопрос
+        movies: {},  // в это свойство поместить пустой объект
+        actors: {},  //тоже поместить пустой объект
+        genres: [] //сюда поместить пустой массив
 };
-console.log(obj.name);
-console.log(obj["name"]);
-
-//массив
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-console.log(arr[1]);
-
-// alert('Hello!');
-
-// const result = confirm("Вы здесь?");
-// console.log(result);
-
-//const answer = +prompt("Вам есть 18?", "18"); // если поставить + то получим числовое значение
-//console.log(typeof(answer));
-
-//const answers = [];  // создали массив
-//answers[0] = prompt('Как Ваше имя?', '');  // присвоение первого элемента
-//answers[1] = prompt('Как Ваше фамилия?', '');
-//answers[2] = prompt('Сколько Вам лет?', '');
-//document.write(answers);  // вывод массива
+    console.log(personalMovieDB.count);
 
 
-// Интерполяция
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
-alert(`https://someurl.com/${category}/5`);
+/*  3) Задайте пользователю по два раза вопросы:
+        - 'Один из последних просмотренных фильмов?'
+        - 'На сколько оцените его?'
+    Ответы стоит поместить в отдельные переменные
+    Записать ответы в объект movies в формате: 
+        movies: {
+            'logan': '8.1'
+        }
+    Проверить, чтобы все работало без ошибок в консоли */
 
+    let lostFilms = prompt("Один из последних просмотренных фильмов?");
+    console.log(lostFilms);
 
+    let ocenka = prompt("На сколько оцените его?");
+    console.log(ocenka);
 
-const user = "Ivan";
-alert(`Привет, ${user}`);
+    movies: {
+        //'logan': '8.1'
+        lostFilms,
+        ocenka
+    };
 
-
-console.log(4 + +"5"); // унарный + преобразует строку в число
-
-let incr = 10,
-    decr = 10;
-
-incr++;
-decr--;
-console.log(incr);
-console.log(decr);
-
-console.log(2*4 === '8'); //строгое равенство
-
-const isChecked = false,
-    isClose = false;
-// console.log(isChecked && isClose);
-console.log(isChecked || !isClose);
+    console.log(movies.ocenka);
